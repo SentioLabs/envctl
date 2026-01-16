4e59621f3c98251338c46eec72ae71f31400f9e9
Ben Firestone <ben.firestone@krypticlabs.com>
2026-01-15 17:18:26 -0800

    feat: add fallback version detection for go install
    Use debug.ReadBuildInfo() to detect version, commit, and build time
    when ldflags aren't set. This enables proper version display when
    installed via: go install github.com/sentiolabs/envctl/cmd/envctl@v0.1.0

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,7 +1,10 @@
 // Package version contains build-time version information.
 package version
 
+import "runtime/debug"
+
 // These variables are set at build time via -ldflags.
+// If not set, we attempt to read from Go module info (for go install @version).
 var (
 	// Version is the semantic version (e.g., "v0.1.0")
 	Version = "dev"
@@ -12,3 +15,31 @@ var (
 	// BuildDate is the build timestamp in RFC3339 format
 	BuildDate = "unknown"
 )
+
+func init() {
+	// If ldflags weren't set, try to read from Go module build info.
+	// This works when installed via: go install ...@v0.1.0
+	if Version == "dev" {
+		if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "" && info.Main.Version != "(devel)" {
+			Version = info.Main.Version
+		}
+	}
+
+	// Try to get VCS info from build settings
+	if GitCommit == "unknown" || BuildDate == "unknown" {
+		if info, ok := debug.ReadBuildInfo(); ok {
+			for _, setting := range info.Settings {
+				switch setting.Key {
+				case "vcs.revision":
+					if GitCommit == "unknown" && len(setting.Value) >= 7 {
+						GitCommit = setting.Value[:7]
+					}
+				case "vcs.time":
+					if BuildDate == "unknown" {
+						BuildDate = setting.Value
+					}
+				}
+			}
+		}
+	}
+}
